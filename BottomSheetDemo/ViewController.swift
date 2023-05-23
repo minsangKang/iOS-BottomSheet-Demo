3352dd5d2f5cdcc39f6c479b5b4df916a217a440
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-05-23 22:42:26 +0900

    feat: viewController: button 추가

--- BottomSheetDemo/ViewController.swift
+++ BottomSheetDemo/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    private let button: UIButton = {
+        let button = UIButton(configuration: .filled())
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        button.setTitle("Show BottomSheet", for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureUI()
+        self.configureButton()
     }
-
-
 }
 
+extension ViewController {
+    private func configureUI() {
+        self.view.addSubview(self.button)
+        NSLayoutConstraint.activate([
+            self.button.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+            self.button.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+    }
+    
+    private func configureButton() {
+        self.button.addAction(UIAction(handler: { [weak self] _ in
+            self?.showBottomSheet()
+        }), for: .touchUpInside)
+    }
+    
+    private func showBottomSheet() {
+        // MARK: Show BottomSheetViewController
+        print("Show BottomSheetViewController")
+    }
+}
